871ed1f2f7fc61902af7ae835306935fcbed7375
JTMCDev <151415208+JTMCDev@users.noreply.github.com>
2023-12-19 15:45:16 +0000

    Stuff

--- main.py
+++ main.py
@@ -6,13 +6,17 @@ import json
 with open('config.json', 'r') as config_file:
     config = json.load(config_file)
 
-intents = discord.Intents.all()
-bot = commands.Bot(command_prefix=config["command_prefix"], intents=intents)
+bot = commands.Bot(command_prefix="!",intents=discord.Intents.all()) #intents are required depending on what you wanna do with your bot
 
 @bot.event
 async def on_ready():
     print("Your bot is online and ready (logged in as {bot.user.name})")
-    await bot.change_presence(activity=discord.Game(name="https://github.com/stanmarshvr/PlayFab-Admin-API-Discord-Bot"))
+    await bot.change_presence(activity=discord.Activity(type=discord.ActivityType.watching, name="/help"))
+    await bot.tree.sync() #sync the command tree
+    help_command=None
+    bot.remove_command('help')
+    print("Bot is ready and online")
+
 
 def make_playfab_request(path, payload=None, method='post'):
     url = f'https://{config["playfab_title_id"]}.playfabapi.com/{path}'
@@ -34,20 +38,33 @@ def is_allowed_role(ctx):
     allowed_role_id = int(config["allowed_role_id"])
     return any(role.id == allowed_role_id for role in ctx.author.roles)
 
-@bot.command()
+@bot.hybrid_command(name="ban")
 @commands.check(is_allowed_role)
-async def ban(ctx, player_id, reason, length):
+async def ban(ctx, player_id : str , reason : str , length : int):
     payload = {
-        'BannedUntil': length,
-        'BanReason': reason,
-        'Permanent': False,
+        "Bans": [{
+        'DurationInHours': length,
+        'Reason': reason,
         'PlayFabId': player_id
+        }]
     }
-    response = make_playfab_request('Admin/BanUsers', payload)
-    if response and not response.get('200'):
-        await ctx.send(f'Banned player {player_id} for {length} due to {reason}')
+    target_role_id = 1183093556710223912
+
+    # Check if the user has the specified role
+    if discord.utils.get(ctx.author.roles, id=target_role_id):
+        response = make_playfab_request('Admin/BanUsers', payload)
+        if response and not response.get('200'):
+            await ctx.send(f'Banned player {player_id} for {length} due to {reason}')
+        else:
+            await ctx.send('Failed to ban the player.')
     else:
-        await ctx.send('Failed to ban the player.')
+        await ctx.send(f"You don't have the required role.")
+    
+    
+
+@bot.hybrid_command(name="helpa", description="Show's what the bot can do, (so far)")
+async def helpa(ctx): 
+   await ctx.send("# Hey! Welcome to my Bot\nMade By JTMC\n# ❗❗❗ALERT! YOU WILL NEED THE ROLE ID IN ORDER TO USE THESE COMMANDS❗❗❗\n\n\n/ban <player_id> <reason> <DurationInHours> This Simply Bans a user from your playfab title")
 
 @bot.command()
 @commands.check(is_allowed_role)
